1e627bd8dfe9d35c4d858825d990957ea037571b
luis carlos <luisestrela.bol951@gmail.com>
2019-12-03 08:35:01 -0300

    integrted with API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './components/Header';
 
+
+
 import './style.css';
+import Main from "./pages/main";
 
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
